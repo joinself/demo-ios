34c17ca61a875361331f9341cc229d1230163f9b
Mark Newman <mark@joinself.com>
2025-07-15 15:58:53 +0100

    initial commit

---
+++ ui-components/CredentialActionType.swift
@@ -0,0 +1,13 @@
+//
+//  CredentialActionType.swift
+//  Example
+//
+//  Created by Long Pham on 18/6/25.
+//
+
+
+public enum CredentialActionType {
+    case emailAddress
+    case identityDocument
+    case customCredential
+}
